d28f8a0d049cb1f055d738be0e94b4fa26ebb5d4
Yann <37202740+Canadasunyan@users.noreply.github.com>
2020-04-29 23:54:53 -0400

    20200429

---
+++ 6-除数游戏.py
@@ -0,0 +1,36 @@
+# 除数游戏
+# A、B两人博弈，A先写一个整数N, 之后每人写一个数字x, 0 < x < N, N % x == 0, 并更新N = N - x
+# 另一方写不出数字则获胜
+# 递归方法
+# 初始化一个向量记录各个值是否被计算过, -1代表未计算, 0代表输, 1代表赢
+def can_win(n):
+    global vector
+    if n == 1:
+        return False
+    if n == 2:
+        return True
+    else:
+        is_win = False
+        for i in range(1, n):
+            # 在所有能整除的的i当中, 只要有一个能够使得对方获胜, 则己方失败
+            if n % i == 0:
+                is_win = is_win | (not can_win(n - i))
+        vector[n] = is_win
+        return vector[n]
+def divisor_game(n):
+    global vector
+    vector = [-1] * (n + 1)
+    if n == 1:
+        print('A获胜！')
+    else:
+        vector[1] = False
+        vector[2] = True
+        can_win(n)
+        if vector[n]:
+            print('B获胜！')
+        else:
+            print('A获胜！')
+
+divisor_game(40)
+
+# 法二: 数学法, 奇数为A胜, 偶数为B胜
